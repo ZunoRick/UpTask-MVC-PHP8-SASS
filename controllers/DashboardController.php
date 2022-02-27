7fa70399868eb2a46cdf6102ba8ba2057ff76514
Ricardo Zuno <zunosan.ricardo506@gmail.com>
2022-02-27 00:36:05 -0600

    Creando proyectos desde el Dashboard

--- controllers/DashboardController.php
+++ controllers/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace Controllers;
 
+use Model\Proyecto;
 use MVC\Router;
 
 class DashboardController{
@@ -15,11 +16,55 @@ class DashboardController{
   }
 
   public static function crear_proyeto(Router $router){
+    $alertas = [];
+    session_start();
+    isAuth();
+    
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+      $proyecto = new Proyecto($_POST );
+      
+      //Validación
+      $alertas = $proyecto->validarProyecto();
+      
+      if (empty($alertas)) {
+        //Generar una url única
+        $hash = md5(uniqid());
+        $proyecto->url = $hash;
+
+        //Almacenar el creador del proyecto
+        $proyecto->propietarioId = $_SESSION['id'];
+
+        //Guardar el proyecto
+        $proyecto->guardar();
+
+        //Redireccionar
+        header('Location: /proyecto?id='.$proyecto->url);
+      }
+    }
+    
+    $router->render('dashboard/crear-proyecto', [
+      'titulo' => 'Crear Proyecto',
+      'alertas' => $alertas
+    ]);
+  }
+
+  public static function proyecto(Router $router){
+    $alertas = [];
     session_start();
     isAuth();
 
-    $router->render('dashboard/crear-proyecto', [
-      'titulo' => 'Crear Proyecto'
+    $url = $_GET['id'];
+    if(!$url) header('Location: /dashboard');
+
+    //Revisar que la persona que visita el proyecto, es quien lo creó
+    $proyecto = Proyecto::where('url', $url);
+
+    if($proyecto->propietarioId !== $_SESSION['id']){
+      header('Location: /dashboard');
+    }
+    
+    $router->render('dashboard/proyecto', [
+      'titulo' => $proyecto->proyecto
     ]);
   }
 
--- public/index.php
+++ public/index.php
@@ -31,6 +31,8 @@ $router->get('/confirmar', [LoginController::class, 'confirmar']);
 //Zona de Proyectos
 $router->get('/dashboard', [DashboardController::class, 'index']);
 $router->get('/crear-proyecto', [DashboardController::class, 'crear_proyeto']);
+$router->post('/crear-proyecto', [DashboardController::class, 'crear_proyeto']);
+$router->get('/proyecto', [DashboardController::class, 'proyecto']);
 $router->get('/perfil', [DashboardController::class, 'perfil']);
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
