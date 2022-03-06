8617a6a6c4386dbe526d965d7e35e6fc505cd6f6
Ricardo Zuno <zunosan.ricardo506@gmail.com>
2022-03-05 22:51:41 -0600

    Agregando eliminar tareas

--- controllers/TareaController.php
+++ controllers/TareaController.php
@@ -83,6 +83,28 @@ class TareaController{
 
   public static function eliminar(){
     if($_SERVER['REQUEST_METHOD'] === 'POST'){
+      //Validar que el proyecto existe
+      $proyecto = Proyecto::where('url', $_POST['proyectoId']);
+
+      session_start();
+      if (!$proyecto || $proyecto->propietarioId !== $_SESSION['id']) {
+        $respuesta = [
+          'tipo' => 'error',
+          'mensaje' => 'Hubo un error al actualizar la tarea'
+        ];
+        echo json_encode($respuesta);
+        return;
+      }
+
+      $tarea = new Tarea($_POST);
+      $resultado = $tarea->eliminar();
+
+      $resultado = [
+        'resultado' => $resultado,
+        'mensaje' => 'Tarea Eliminada Correctamente'
+      ];
+
+      echo json_encode($resultado);
     }
   }
 }
