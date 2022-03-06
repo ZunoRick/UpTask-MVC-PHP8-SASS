e39a0ace5ea83c6b26a23af0c7ff133e0ef7e2fe
Ricardo Zuno <zunosan.ricardo506@gmail.com>
2022-03-06 12:58:46 -0600

    Agregando filtros de tareas completadas y pendientes

--- views/dashboard/proyecto.php
+++ views/dashboard/proyecto.php
@@ -9,6 +9,26 @@
       >&#43; Nueva Tarea</button>
     </div>
 
+    <div id="filtros" class="filtros">
+      <div class="filtros-inputs">
+        <h2>Filtros:</h2>
+        <div class="campo">
+          <label for="todas">Todas</label>
+          <input type="radio" name="filtro" id="todas" value="" checked>
+        </div>
+
+        <div class="campo">
+          <label for="completadas">Completadas</label>
+          <input type="radio" name="filtro" id="completadas" value="1">
+        </div>
+
+        <div class="campo">
+          <label for="pendientes">Pendientes</label>
+          <input type="radio" name="filtro" id="pendientes" value="0">
+        </div>
+      </div>
+    </div>
+
     <ul class="listado-tareas" id="listado-tareas"></ul>
   </div>
 
