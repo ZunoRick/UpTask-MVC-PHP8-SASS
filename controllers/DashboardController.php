bbe4418b7a1025f50fee1e5fa5303ff74eb0263b
Ricardo Zuno <zunosan.ricardo506@gmail.com>
2022-03-07 18:39:32 -0600

    Agregando actualización de perfil, email y password

--- controllers/DashboardController.php
+++ controllers/DashboardController.php
@@ -3,6 +3,7 @@
 namespace Controllers;
 
 use Model\Proyecto;
+use Model\Usuario;
 use MVC\Router;
 
 class DashboardController{
@@ -74,9 +75,84 @@ class DashboardController{
   public static function perfil(Router $router){
     session_start();
     isAuth();
+    $alertas = [];
+
+    $usuario = Usuario::find($_SESSION['id']);
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+      $usuario->sincronizar($_POST);
+      $alertas = $usuario->validar_perfil();
+
+      if (empty($alertas)) {
+        $existeUsuario = Usuario::where('email', $usuario->email);
+
+        if ($existeUsuario && $existeUsuario->id !== $usuario->id) {
+          Usuario::setAlerta('error', 'Correo ya registrado');
+          $alertas = $usuario->getAlertas();
+        } else{
+          //guardar el usuario
+          $usuario->guardar();
+  
+          Usuario::setAlerta('exito', 'Guardado Correctamente');
+          $alertas = $usuario->getAlertas();
+  
+          //Asignar el nombre nuevo a la barra
+          $_SESSION['nombre'] = $usuario->nombre;
+        }
+      }
+    }
 
     $router->render('dashboard/perfil', [
-      'titulo' => 'Editar Perfil'
+      'titulo' => 'Editar Perfil',
+      'usuario' => $usuario,
+      'alertas' => $alertas
+    ]);
+  }
+
+  public static function cambiar_password(Router $router){
+    session_start();
+    isAuth();
+
+    $alertas = [];
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+      $usuario = Usuario::find($_SESSION['id']);
+      
+      //Sincronizar con los datos del usuario
+      $usuario->sincronizar($_POST);
+
+      $alertas = $usuario->nuevo_password();
+
+      if (empty($alertas)) {
+        $resultado = $usuario->comprobar_password();
+        
+        if($resultado){
+          $usuario->password = $usuario->password_nuevo;
+
+          //Eliminar propiedades no necesarias
+          unset($usuario->password_actual);
+          unset($usuario->password_nuevo);
+
+          //Hashear el nuevo password
+          $usuario->hashPassword();
+
+          //actualizar
+          $resultado = $usuario->guardar();
+
+          if ($resultado) {
+            Usuario::setAlerta('exito', 'Password Guardado Correctamente');
+            $alertas = Usuario::getAlertas();
+          }
+        }else{
+          Usuario::setAlerta('error', 'Password Incorrecto');
+          $alertas = Usuario::getAlertas();
+        }
+      }
+    }
+    
+    $router->render('dashboard/cambiar-password', [
+      'titulo' => 'Cambiar Password',
+      'alertas' => $alertas
     ]);
   }
 }
--- views/dashboard/perfil.php
+++ views/dashboard/perfil.php
@@ -1,5 +1,35 @@
 <?php include_once __DIR__.'/header-dashboard.php'; ?>
 
+  <div class="contenedor-sm">
+    <?php include_once __DIR__.'/../templates/alertas.php'; ?>
 
+    <a href="/cambiar-password" class="enlace">Cambiar Password</a>
+
+    <form action="/perfil" class="formulario" method="POST">
+      <div class="campo">
+        <label for="nombre">Nombre:</label>
+        <input 
+          type="text" 
+          name="nombre" 
+          id="nombre" 
+          value="<?php echo $usuario->nombre; ?>" 
+          placeholder="Tu Nombre"
+        />
+      </div>
+
+      <div class="campo">
+        <label for="email">Email:</label>
+        <input 
+          type="email" 
+          name="email" 
+          id="email" 
+          value="<?php echo $usuario->email; ?>" 
+          placeholder="Tu Email"
+        />
+      </div>
+
+      <input type="submit" value="Guardar Cambios">
+    </form>
+  </div>
 
 <?php include_once __DIR__.'/footer-dashboard.php'; ?>
